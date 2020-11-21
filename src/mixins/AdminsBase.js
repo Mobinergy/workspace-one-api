9a47f368ef44644581a12cfa4b683e9875db31a7
Yaaqoub <semlali.yaaqoub@gmail.com>
2020-11-21 13:51:34 +0100

    change admin password by ID and Uuid

--- src/mixins/AdminsBase.js
+++ src/mixins/AdminsBase.js
@@ -3,6 +3,24 @@ const _ = require('lodash');
 
 const adminsBase = {
 
+    async changePasswordById(id, body) {
+        this.options.url += `${id}/changepassword`;
+        this.options.method = 'POST';
+        this.options.data = body;
+
+        let response = await axios(this.options);
+        return response.data;
+    },
+
+    async changePasswordByUuid(uuid, body) {
+        this.options.url += `${uuid}/password`;
+        this.options.method = 'PUT';
+        this.options.headers.Accept += 'version=2';
+        this.options.data = body;
+
+        let response = await axios(this.options);
+        return response.data;
+    },
 };
 
 module.exports = adminsBase;
